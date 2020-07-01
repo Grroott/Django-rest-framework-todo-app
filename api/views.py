7bc648ead6acfbd4132402168bb1cdca34f431fd
Gokul nath <gokulnath634@gmail.com>
2020-07-01 10:11:56 +0530

    Using Serializer to serialise the DB data & returns as JSON

--- api/views.py
+++ api/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+from .serializers import TaskSerializer
+from .models import Task
 # Create your views here.
 @api_view(['GET'])
 def apiOverview(request):
@@ -18,3 +18,10 @@ def apiOverview(request):
 	}
 
 	return Response(context)
+
+@api_view(['GET'])
+def taskList(request):
+
+	tasks = Task.objects.all()
+	serializer = TaskSerializer(tasks, many=True)
+	return Response(serializer.data)
