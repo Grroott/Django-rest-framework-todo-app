1ba4171f4790b250339b44a9309fb67340f89cb4
Gokul nath <gokulnath634@gmail.com>
2020-07-01 09:53:51 +0530

    Static data returned through the response

--- api/views.py
+++ api/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+# Create your views here.
+@api_view(['GET'])
 def apiOverview(request):
-	return JsonResponse("API BASE POINT", safe=False)
+
+	context={
+		"id": 1,
+  "first_name": "Jeanette",
+  "last_name": "Penddreth",
+  "email": "jpenddreth0@census.gov",
+  "gender": "Female",
+  "ip_address": "26.58.193.2",
+	}
+
+	return Response(context)
